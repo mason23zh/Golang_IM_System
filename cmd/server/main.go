00246f41e8bfb2a3047953909670fbde19edb65c
mason23zh <mason.zh@gmail.com>
2026-07-15 18:21:52 -0500

    added cli flag parser for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,18 @@
 package main
 
+import "flag"
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "setting server IP address")
+	flag.IntVar(&serverPort, "port", 8080, "setting server port number")
+}
+
 func main() {
-	server := NewServer("127.0.0.1", 8080)
+	flag.Parse()
+
+	server := NewServer(serverIp, serverPort)
 	server.Start()
 }
